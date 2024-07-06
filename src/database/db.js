450468892077c7172a566f32288cafa5a870b489
Domegal13 <Domegal13cursos@gmail.com>
2024-07-06 20:42:43 -0300

    Se arreglan las variables de entorno

--- src/database/db.js
+++ src/database/db.js
@@ -5,7 +5,15 @@ import mysql from "../../config.js";
 const db = new Sequelize(mysql.database, mysql.user, "", {
   host: mysql.host,
   dialect: "mysql",
-  port: 3306,
+  port: mysql.DB_PORT,
 });
 
 export default db;
+
+// const db = new Sequelize(mysql.DB_NAME, mysql.DB_USER, mysql.DB_PASSWORD, {
+//   host: mysql.DB_HOST,
+//   dialect: "mysql",
+//   port: mysql.DB_PORT,
+// });
+
+// export default db;
