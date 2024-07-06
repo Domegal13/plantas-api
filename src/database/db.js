e03304d2fecbbcfe7330894ea2bb3e5884c0e9dd
Domegal13 <Domegal13cursos@gmail.com>
2024-07-06 15:36:09 -0300

    se eliminan variables no utilizadas

--- src/database/db.js
+++ src/database/db.js
@@ -4,15 +4,8 @@ import mysql from "../../config.js";
 
 const db = new Sequelize(mysql.database, mysql.user, "", {
   host: mysql.host,
-  // host: "localhost",
   dialect: "mysql",
   port: 3306,
 });
-
-// const db = new Sequelize("plantas_api", "root", "", {
-//   host: "localhost",
-//   dialect: "mysql",
-//   port: 3306,
-// });
 
 export default db;
